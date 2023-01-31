57d4a4b236ac1b72397fca870c9238943f9a35e7
Vycheslav Tumanov <tumanov@umarsh.ru>
2023-01-31 21:52:07 +0300

    add modules DI

---
+++ app/src/main/java/ru/umarsh/weatherapp/di/LocationModule.kt
@@ -0,0 +1,18 @@
+package ru.umarsh.weatherapp.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import ru.umarsh.weatherapp.data.location.DefaultLocationTracker
+import ru.umarsh.weatherapp.domen.location.LocationTracker
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class LocationModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindLocationTracker(defaultLocationTracker: DefaultLocationTracker): LocationTracker
+}
---
+++ app/src/main/java/ru/umarsh/weatherapp/di/RepositoryModule.kt
@@ -0,0 +1,18 @@
+package ru.umarsh.weatherapp.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import ru.umarsh.weatherapp.data.repository.WeatherRepositoryImpl
+import ru.umarsh.weatherapp.domen.repository.WeatherRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindWeatherRepository(weatherRepositoryImpl: WeatherRepositoryImpl): WeatherRepository
+}
